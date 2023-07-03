1801bc7379d6924b82da7ecec5d2c7ddbd557482
mazeltovik <mazeltovik@gmail.com>
2023-07-03 12:57:04 +0300

    refactor: add simple multi server

--- src/server.ts
+++ src/server.ts
@@ -8,59 +8,60 @@ const server = http.createServer(handlers);
 export default server;
 
 export function multiServer(port: string | number) {
-    if (cluster.isPrimary) {
-        const cores = availableParallelism() - 1;
-        const ports: number[] = [];
-        for (let i = 0; i < cores; i++) {
-            let workerPort = Number(port) + 1 + i;
-            cluster.fork({ WORKER_PORT: workerPort });
-            ports.push(workerPort);
-        }
-
-        let next = 0;
-
-        const multiServer = http.createServer((req, res) => {
-            multiServer.on("error", (error) => {
-                console.log(error.message);
-            });
-            console.log(`This is request ${req.method}: ${req.url} to Multi Server`);
-            next = (next + 1) % ports.length;
-            const availablaPort = Number(port) + next;
-            try {
-                const reqToAvailblePort = http.request(
-                    {
-                        hostname: "localhost",
-                        path: req.url,
-                        method: req.method,
-                        headers: req.headers,
-                        port: availablaPort,
-                    },
-                    (response) => {
-                        res.writeHead(
-                            response.statusCode ?? 200,
-                            response.statusMessage,
-                            response.headers
-                        );
-                        response.pipe(res);
-                    }
-                );
-                reqToAvailblePort.on("error", (error) => {
-                    console.log(error.message);
-                    req.unpipe(reqToAvailblePort);
-                });
-
-                console.log(`Server on port ${availablaPort} is answering`);
-
-                req.pipe(reqToAvailblePort);
-            } catch (error) {
-                console.log(error);
-            }
-        });
-        multiServer.listen(port, () => {
-            console.log(`Multi Server is running on port ${port}`);
-        });
-    } else {
-        let workerPort = process.env.WORKER_PORT? Number(process.env.WORKER_PORT): 4001;
-        server.listen(workerPort);
+    const cores = availableParallelism() - 1;
+    const workerPorts: number[] = [];
+    for (let i = 0; i < cores; i++) {
+        let workerPort = Number(port) + 1 + i;
+        workerPorts.push(workerPort);
     }
+    workerPorts.forEach(port => {
+        http.createServer(handlers).listen(port, () => {
+            console.log(`Server is running on ${port}`);
+        })
+    })
+
+    let next = 0;
+    const multiServer = http.createServer((req, res) => {
+        multiServer.on("error", (error) => {
+            console.log(error.message);
+        });
+        console.log(`This is request ${req.method}: ${req.url} to Multi Server`);
+        next = (next + 1) % workerPorts.length;
+        if(next == 0){
+            next = 7
+        }
+        const availablaPort = Number(port) + next;
+        try {
+            const reqToAvailblePort = http.request(
+                {
+                    hostname: "localhost",
+                    path: req.url,
+                    method: req.method,
+                    headers: req.headers,
+                    port: availablaPort,
+                },
+                (response) => {
+                    res.writeHead(
+                        response.statusCode ?? 200,
+                        response.statusMessage,
+                        response.headers
+                    );
+                    response.pipe(res);
+                }
+            );
+            reqToAvailblePort.on("error", (error) => {
+                console.log(error.message);
+                req.unpipe(reqToAvailblePort);
+            });
+
+            console.log(`Server on port ${availablaPort} is answering`);
+
+            req.pipe(reqToAvailblePort);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+    multiServer.listen(port, () => {
+        console.log(`Multi Server is running on port ${port}`);
+    });
 };
